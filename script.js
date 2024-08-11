21d1454d335ef8dce41562fce26742dbd3b05263
Celielab <celielab@hotmail.com>
2024-08-11 10:48:35 +0200

    fetch data from api

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const form = document.querySelector("form");
 
 const submit = (event) => {
     event.preventDefault();
-    console.log("hello");
-}
+    const amountOfPokemon = (event.target[0].value);
+    let offset;
 
+    const apiLink = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit="+ amountOfPokemon
+
+    fetch(apiLink)
+    .then(response => response.json())
+    .then (data => {
+        for (let i = 0; i< amountOfPokemon; i++ ){
+                let pokemonName = data.results[i].name
+                
+
+        }
+            
+    })
+}
 form.addEventListener("submit", submit);
